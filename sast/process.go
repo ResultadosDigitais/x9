443710e2164ded607298e8d33fce01d0f69d8831
vitoria <vitoria.mdrio@gmail.com>
2020-03-29 23:10:00 -0300

    [REFACT] Remove util folder

--- sast/process.go
+++ sast/process.go
@@ -1,13 +1,17 @@
 package sast
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"os"
+	"path/filepath"
+	"regexp"
 	"time"
 
+	"github.com/ResultadosDigitais/x9/config"
 	"github.com/ResultadosDigitais/x9/git"
 	"github.com/ResultadosDigitais/x9/log"
-	"github.com/ResultadosDigitais/x9/util"
 	"github.com/google/go-github/github"
 )
 
@@ -27,10 +31,13 @@ func (pw *ProcessWorker) ProcessEvent() {
 	for e := range pw.Events {
 		repository, err := pw.Session.GetRepository(e.GetRepo().GetID())
 		url := repository.GetCloneURL()
+		if isBlacklisted(url, config.GetBlacklistedRepositories()) {
+			continue
+		}
 		if err != nil {
 			log.Error(fmt.Sprintf("Error getting repository info: %s", url), map[string]interface{}{"error": err.Error()})
 		}
-		dir := util.GetTempDir(os.TempDir(), util.GetHash(url, time.Now().String()))
+		dir := getDir(url)
 		if _, err := pw.Session.CloneRepository(url, dir); err != nil {
 			log.Error(fmt.Sprintf("Error cloning repository: %s", url), map[string]interface{}{"error": err.Error()})
 
@@ -39,4 +46,46 @@ func (pw *ProcessWorker) ProcessEvent() {
 		pw.Leaks.Test(url, dir)
 		os.RemoveAll(dir)
 	}
+}
+
+func isBlacklisted(repo string, blackListedRepositories []string) bool {
+	for _, blackListedRepo := range blackListedRepositories {
+		if match, err := regexp.MatchString(`^.*(/`+blackListedRepo+`/).*$`, repo); err != nil {
+			log.Error("Regexp error compile", map[string]interface{}{"error": err.Error()})
+		} else if match == true {
+			return true
+		}
+	}
+	return false
+
+}
+
+func getDir(url string) string {
+	folderName := getHash(url, time.Now().String())
+	return getTempDir(os.TempDir(), folderName)
+
+}
+
+func getTempDir(tempDir, suffix string) string {
+	dir := filepath.Join(tempDir, suffix)
+
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.MkdirAll(dir, os.ModePerm)
+	} else {
+		os.RemoveAll(dir)
+	}
+
+	return dir
+}
+
+// getHash receives one or more strings and returns
+// a sha1 hash of the concatenation of all of them
+func getHash(values ...string) string {
+	str := values[0]
+	for i := 1; i < len(values); i++ {
+		str = fmt.Sprintf("%s%s", str, values[i])
+	}
+	hashString := sha1.New()
+	hashString.Write([]byte(str))
+	return hex.EncodeToString(hashString.Sum(nil))
 }
