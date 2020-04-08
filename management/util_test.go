f0db2b36d76c9978c2aa1dbaeb2bdebbd0188bd9
vitoria <vitoria.mdrio@gmail.com>
2020-04-08 02:24:05 -0300

    [REFACT] Remove internal ID

--- management/util_test.go
+++ management/util_test.go
@@ -6,11 +6,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetInternalID(t *testing.T) {
+func TestGetID(t *testing.T) {
 
 	expected := "5e825fe33b90934ae809cd79688f9f4da07b0a357b3062e3e892ea183125d511"
 
-	resp := getInternalID(1, true, "abc")
+	resp := getID(1, true, "abc")
 
 	assert.Equal(t, expected, resp)
 
